b136208b8dd37d4a7019b321bef4064d1fdabe34
小夏 <316854001@qq.com>
2019-11-19 11:16:12 +0800

    2019年11月19日11:10:02

--- app/src/main/java/com/ok100/weather/wxapi/WXEntryActivity.java
+++ app/src/main/java/com/ok100/weather/wxapi/WXEntryActivity.java
@@ -2,15 +2,6 @@ package com.ok100.weather.wxapi;
 
 import com.umeng.socialize.weixin.view.WXCallbackActivity;
 
-/**
- * @author Administrator QQ:316854001
- * @name Weahter
- * @class name：com.ok100.weather.wxapi
- * @class describe
- * @time 2019/11/19 9:29
- * @change
- * @chang time
- * @class describe
- */
+
 public class WXEntryActivity extends WXCallbackActivity {
 }
